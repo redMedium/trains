307b1ed9b1fc3797493f7d15540826cd811a15c7
h6ss4 <73107557+h6ss4@users.noreply.github.com>
2021-11-09 14:24:19 +0200

    lisätty hakukenttä, lähteen jsonin voi tallettaa muuttujaan

--- js.js
+++ js.js
@@ -1,3 +1,12 @@
+document.querySelector("#searchSubmit").addEventListener("click",    // submit button event, when selected, ...
+    () => {    // ... calls an anonymous function
+        var searchText = document.querySelector("#searchText").value;    // save the search field input text to a variable
+        var respArr = [];
 
+        fetch("https://rata.digitraffic.fi/api/v1/live-trains/station/HKI/TPE")
+            .then(resp => resp.json())
+            .then(data => respArr.push(data));
 
-document.getElementById("x").innerHTML = ;
+        console.log(respArr);
+    }
+);
