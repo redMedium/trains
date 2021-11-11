06b4000d12819c282d54cd6ef3452d2647402f41
redMedium <89989778+redMedium@users.noreply.github.com>
2021-11-11 13:13:40 +0200

    selvitä promise, miten siitä saa lypsettyä vastauk

--- js.js
+++ js.js
@@ -1,12 +1,18 @@
-document.querySelector("#searchSubmit").addEventListener("click",    // submit button event, when selected, ...
-    () => {    // ... calls an anonymous function
-        var searchText = document.querySelector("#searchText").value;    // save the search field input text to a variable
-        var respArr = [];
+const stations = fetch("https://rata.digitraffic.fi/api/v1/metadata/stations")
+    .then(response => response.json())
+    .then(data => {return data});
+
+console.log(stations);
+
+document.querySelector("#searchSubmit").addEventListener("click",                           // Submit button event, when selected, ...
+    () => {                                                                                 // ... calls an anonymous function.
+        var searchText = document.querySelector("#searchText").value;                       // Save the search field input text to a variable.
+        var trainArr = [];
 
         fetch("https://rata.digitraffic.fi/api/v1/live-trains/station/HKI/TPE")
-            .then(resp => resp.json())
-            .then(data => respArr.push(data));
+            .then(response => response.json())
+            .then(data => trainArr.push(data));
 
-        console.log(respArr);
+        console.log(trainArr);
     }
 );
